a9dcdbf2afbbee7be283698af3026f0e9bf7de98
Patrick <patrickrx7@gmail.com>
2019-07-13 10:23:46 -0600

    REST API
    REST API para recetas implementado. Con sus métodos GET, POST y DELETE respectivos.

--- Backend/models/Recipe.js
+++ Backend/models/Recipe.js
@@ -4,7 +4,7 @@ const RecipeSchema = new Schema({ //Esquema o modelo para los datos de las recet
     title: {type: String, required: true},
     ingredients: {type: String, required: true},
     instructions: {type: String, required: true},
-    imagePath: {type: String, required: true}, //para las imagenes solo guardamos la dirección.
+    imagePath: {type: String}, //para las imagenes solo guardamos la dirección.
     created_at: {type: Date, default: Date.now} 
 });
 
--- Backend/routes/recipes.js
+++ Backend/routes/recipes.js
@@ -1,6 +1,26 @@
 const { Router } = require('express');
 const router = Router(); //Este enrutador sirve para definir las rutas del servidor.
 
-router.get('/', (req, res) => res.json({text: 'Hello World'}));
+const Recipe = require('../models/Recipe');
+
+router.get('/', async (req, res) => { //Con get recuperamos todas las recetas.
+    const recipes = await Recipe.find() //Este find busca todas las recetas de la base de datos. 
+                        //Es como un select * en SQL.
+    res.json(recipes);
+});
+
+router.post('/', async(req, res) => { //Utilizamos post para enviar nuevas recetas.
+    const { title, ingredients, instructions } = req.body; //req body es lo que se recibe en el post.
+    const newRecipe = new Recipe({title, ingredients, instructions}); //creamos una nueva receta con el modelo.
+    console.log(newRecipe);
+    await newRecipe.save(); //Se guarda la receta en la base de datos MongoDB.
+    res.json({message: 'Recipe saved'});
+});
+
+router.delete('/:id', async (req, res) => { //Usamos delete para borrar una receta utilizando su id.
+    const recipe = await Recipe.findByIdAndDelete(req.params.id);
+    console.log(recipe);
+    res.json({message: 'Recipe deleted'});
+});
 
 module.exports = router;
